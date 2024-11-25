be84461a6e45ef6b3f706210c777a24f60d19fb9
Leon Cugura <leoncugura@gmail.com>
2024-11-25 15:24:44 +0100

    Created routes.

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -11,6 +11,17 @@ class _ChooseLocationState extends State<ChooseLocation> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        titleTextStyle: TextStyle(
+          color: Colors.white,
+          fontSize: 25,
+        ),
+        backgroundColor: Colors.blue[900],
+        title: Text('Choose location'),
+        centerTitle: true,
+        elevation: 0,
+      ),
       body: Text('choose location screen'),
     );
   }
