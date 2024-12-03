ae01e06c22a196104f77d1df48267dd3c24accf4
Leon Cugura <leoncugura@gmail.com>
2024-12-03 14:48:25 +0100

    Understanding async code and http requests.

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:worldtime_weather/pages/choose_location.dart';
 import 'package:worldtime_weather/pages/home.dart';
 import 'package:worldtime_weather/pages/loading.dart';
+import 'package:http/http.dart';
 
 void main() => runApp(MaterialApp(
-  initialRoute: '/home',
+  initialRoute: '/',
   routes: {
     '/': (context) => Loading(),
     '/home': (context) => Home(),
--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
 
 class Loading extends StatefulWidget {
   const Loading({super.key});
@@ -8,6 +12,19 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+
+  void getData() async {
+    Response response = await get(Uri.parse('https://jsonplaceholder.typicode.com/todos/1'));
+    Map data = jsonDecode(response.body);
+    print(data['title']);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
